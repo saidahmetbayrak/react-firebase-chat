65b32841adfad848c22c6d127f9191b56822619f
saidahmetbayrak <saidahmetbayrak@hotmail.com>
2025-08-06 19:29:57 +0300

    fix: Handle null case for chats in Chats.js

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -15,7 +15,7 @@ const Chats = () => {
   useEffect(() => {
     const getChats = () => {
       const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-        setChats(doc.data());
+        setChats(doc.data() || {});
       });
 
       return () => {
